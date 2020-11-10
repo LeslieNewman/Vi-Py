8476ea1f829d7513a36d9ec7e00c7826d5a4dc8e
LeslieNewman <lcnewman@alum.mit.edu>
2020-11-10 14:00:49 -0500

    Create def.py

---
+++ py/def.py
@@ -0,0 +1,46 @@
+#-----
+#  Imports
+#-----
+import urllib, urllib2, json
+
+
+#-----
+#  getUrlPrefix   - returns the service access path
+#  runvx(vision_expression) - pure vision, pass in a vision expression, do your own encoding
+#  runvision ( {'expression' : vision_expression} ) - pass in a py dictionary that presents vision expression
+#  runvisionJSON ({'expression' : vision_expression} ) - pass in a py dictionary, vision returns json object
+#-----
+
+#---  getUrlPrefix
+def getUrlPrefix():
+   return('http://ip-172-31-28-41.us-east-2.compute.internal:10000/vision/api')
+
+#---  runvx("Schema+showInheritance")
+def runvx(query):
+   url=getUrlPrefix()
+   request = url + '?expression=' + query
+   response=urllib2.urlopen(request)
+   return response.read().decode('ascii')
+
+#--- runvision( {'expression' : 'Schema showInheritance'} )
+#--- runvision ('Schema showInheritance')
+def runvision(query):
+   url=getUrlPrefix()
+   if (type(query) is dict):
+       exp = query
+   else:
+       exp = {'expression' : query}
+   request=urllib2.Request(url,urllib.urlencode(exp).encode('ascii'))
+   response=urllib2.urlopen(request)
+   return response.read().decode('ascii')
+
+#--- runvisionJSON( {'expression' : open('./examples/schema.vis','r').read()} )
+#--- runvisionJSON( open('./examples/schema.vis','r').read())
+def runvisionJSON(query):
+     return json.loads(runvision(query))
+
+#--- runapp ("appName", params={})
+def runapp (appName, params={} ) :
+   exp = 'Applay run: "' + appName + '"'
+   params['expression'] = exp
+   return runvisionJSON(params)
